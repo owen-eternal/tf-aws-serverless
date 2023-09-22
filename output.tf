f0ff6330cb01dbf17cdd3a8f8c5d7df377de63bb
owen-eternal <ptarworx@gmail.com>
2023-09-22 09:58:34 +0200

    Create output for the main lamba function

--- output.tf
+++ output.tf
@@ -2,3 +2,7 @@ output "LambdaExecutionRoleArn" {
   value     = aws_iam_role.lambda-role.arn
   sensitive = true
 }
+
+output "TekkenLambdafunction" {
+  value = aws_lambda_function.tekken-lambda-function.id
+}
