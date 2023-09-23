b7166905f6345d3e5675a4ce168d18146bee5a65
owen-eternal <ptarworx@gmail.com>
2023-09-24 00:50:08 +0200

    Integrate API gateway with Lambda

--- apigateway.tf
+++ apigateway.tf
@@ -21,3 +21,12 @@ resource "aws_api_gateway_method" "character-http-method" {
   resource_id   = aws_api_gateway_resource.character-resource.id
   rest_api_id   = aws_api_gateway_rest_api.tekken-rest-api.id
 }
+
+resource "aws_api_gateway_integration" "character-integration" {
+  integration_http_method = "POST"
+  http_method             = aws_api_gateway_method.character-http-method.http_method
+  resource_id             = aws_api_gateway_resource.character-resource.id
+  rest_api_id             = aws_api_gateway_rest_api.tekken-rest-api.id
+  uri                     = aws_lambda_function.tekken-lambda-function.invoke_arn
+  type                    = "AWS_PROXY"
+}
