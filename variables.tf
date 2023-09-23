62ef1903dd77176c140dcc3aadb63f502897dc8e
owen-eternal <ptarworx@gmail.com>
2023-09-24 00:43:29 +0200

    Create region and account_id variables

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  type        = string
+  description = "AWS region resources are created under."
+}
+
+variable "account_id" {
+  type        = string
+  description = "ID of the AWS account."
+}
