e2420a7bcea3ec5763c3d5229fccde81349f16c1
owen-eternal <ptarworx@gmail.com>
2023-09-24 00:50:55 +0200

    Create routing for fetching all characters

--- apigateway.tf
+++ apigateway.tf
@@ -30,3 +30,26 @@ resource "aws_api_gateway_integration" "character-integration" {
   uri                     = aws_lambda_function.tekken-lambda-function.invoke_arn
   type                    = "AWS_PROXY"
 }
+
+#Routing --> /characters endpoint
+resource "aws_api_gateway_resource" "characters-resource" {
+  rest_api_id = aws_api_gateway_rest_api.tekken-rest-api.id
+  parent_id   = aws_api_gateway_rest_api.tekken-rest-api.root_resource_id
+  path_part   = "characters"
+}
+
+resource "aws_api_gateway_method" "characters-http-method" {
+  authorization = "NONE"
+  http_method   = "GET"
+  resource_id   = aws_api_gateway_resource.characters-resource.id
+  rest_api_id   = aws_api_gateway_rest_api.tekken-rest-api.id
+}
+
+resource "aws_api_gateway_integration" "characters-integration" {
+  integration_http_method = "POST"
+  http_method             = aws_api_gateway_method.characters-http-method.http_method
+  resource_id             = aws_api_gateway_resource.characters-resource.id
+  rest_api_id             = aws_api_gateway_rest_api.tekken-rest-api.id
+  uri                     = aws_lambda_function.tekken-lambda-function.invoke_arn
+  type                    = "AWS_PROXY"
+}
