701802958fee13764cb28bbf2a306778ceb9e2ff
owen-eternal <ptarworx@gmail.com>
2023-09-24 00:48:28 +0200

    Create resource for fetching a single character

--- apigateway.tf
+++ apigateway.tf
@@ -7,3 +7,10 @@ resource "aws_api_gateway_rest_api" "tekken-rest-api" {
     types = ["REGIONAL"]
   }
 }
+
+#Routing --> /character endpoint
+resource "aws_api_gateway_resource" "character-resource" {
+  rest_api_id = aws_api_gateway_rest_api.tekken-rest-api.id
+  parent_id   = aws_api_gateway_rest_api.tekken-rest-api.root_resource_id
+  path_part   = "character"
+}
