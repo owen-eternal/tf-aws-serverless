4520105006e9605b9ae1609c97cce2d8e93f3311
owen-eternal <ptarworx@gmail.com>
2023-09-24 00:45:18 +0200

    Map region and Acc ID env vars to local vars

--- lambdas.tf
+++ lambdas.tf
@@ -1,10 +1,19 @@
 ###########################################
+############### LOCALS ####################
+###########################################
+
+locals {
+  api_id     = aws_api_gateway_rest_api.tekken-rest-api.id
+  account_id = var.account_id
+  region     = var.region
+}
+
+###########################################
 ############### RESOURCES #################
 ###########################################
 
 # Lambda for main tekken API.
 resource "aws_lambda_function" "tekken-lambda-function" {
-
   filename         = "${path.module}/app/tekken-handler.zip"
   function_name    = "TekkenLambdaFunction"
   role             = aws_iam_role.lambda-role.arn
